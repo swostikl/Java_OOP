44b4d7bf180b16b8931a87e45d79a87c9d3bedf5
Swostika lama <swostikl@metropolia.fi>
2025-08-18 20:22:48 +0300

    initial commit

---
+++ Module1/src/Exercise1_1.java
@@ -0,0 +1,11 @@
+// Task 1
+//Install the environment, create a new project, and write a simple program that greets the user with a fixed message.
+// Run the program and make sure that it works. Commit your changes to GitHub.
+
+
+public class Exercise1_1 {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+}
