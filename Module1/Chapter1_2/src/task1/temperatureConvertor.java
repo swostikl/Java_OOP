49933d9ee088ddcda246cf2e7370e1510c271aa5
Swostika lama <swostikl@metropolia.fi>
2025-08-19 21:12:17 +0300

    initial commit

---
+++ Module1/Chapter1_2/src/task1/temperatureConvertor.java
@@ -0,0 +1,16 @@
+package task1;
+//Task 1: Fahrenheit to Celsius Converter
+
+import java.util.Scanner;
+public class temperatureConvertor {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter temperature in Fahrenheit: ");
+        double temperature = Double.parseDouble(input.nextLine());
+
+        double celsius = (temperature-32)*5/9;
+        System.out.printf("Celsius : %6.2f  %s\n" , celsius , "degree");
+    }
+}
