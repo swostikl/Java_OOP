e75e168118364a178430fa38b4b2d4e7dcaa2b7f
Swostika lama <swostikl@metropolia.fi>
2025-08-19 18:41:53 +0300

    initial commit

--- Module1/src/Exercise1_1.java
+++ Module1/src/Exercise1_1.java
@@ -5,7 +5,7 @@
 
 public class Exercise1_1 {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println(" Good morning! Swostika !");
     }
 
 }
