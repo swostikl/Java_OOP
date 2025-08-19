fa9d2aa1e73d05c3db4f65b5b92ffb7686f021fd
Swostika lama <swostikl@metropolia.fi>
2025-08-19 18:27:55 +0300

    initial commit

---
+++ Module1/src/Exercise1_2.java
@@ -0,0 +1,16 @@
+//Task 2
+//Write a program that prints the following:
+//       *
+//      ***
+//     *****
+//    *******
+
+
+public class Exercise1_2 {
+    public static void main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("********");
+    }
+}
