85074891ce52c0cccef8fd5d86dd4b7e2154cfa6
Swostika lama <swostikl@metropolia.fi>
2025-08-25 06:23:14 +0300

    initial commit

---
+++ Module1/Chapter1_3/src/task3/primeNumberGenerator.java
@@ -0,0 +1,52 @@
+package task3;
+
+// Write a program that prompts the user to enter two positive integers, start and end (where start < end). The program
+// should generate and display all the prime numbers between start and end, inclusive.
+//A prime number is a positive integer greater than 1 that has no positive divisors other than 1 and itself.
+//Your program should use control structures (such as loops and conditional statements) to implement the logic for
+// generating and checking prime numbers.
+
+
+import java.util.Scanner;
+
+public class primeNumberGenerator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a start number: ");
+        int start = input.nextInt();
+
+        System.out.println("Enter a end number: ");
+        int end = input.nextInt();
+
+        // check validate input
+        if (start >= end || start < 1) {
+            System.out.println("Invalid input, enter the positive number : ");
+        }else {
+            System.out.println("Prime numbers between " + start + " and " + end + " are : ");
+
+            for(int number = start; number <= end; number++){
+                if (isPrime(number)){
+                    System.out.print(number + " ");
+                }
+            }
+
+        }
+
+
+    }
+
+    public static boolean isPrime(int number){
+        if (number <=1){
+            return false;
+        }
+
+        for (int i = 2; i  <= Math.sqrt(number); i++){
+            if (number % i == 0){
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
